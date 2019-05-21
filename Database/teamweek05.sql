9fa44a90091b361694aab0d58e972fc84cecebca
aspayne <pay12008@byui.edu>
2019-05-21 15:24:11 -0600

    teamactivity5

---
+++ Database/teamweek05.sql
@@ -0,0 +1,15 @@
+Create table scriptures
+( id INT PRIMARY KEY
+, book VARCHAR
+, chapter INT
+, verse INT
+, content VARCHAR);
+
+
+Insert into scriptures
+Values(
+1
+, 'John'
+, 1
+, 5
+, 'And the light shineth in darkness; and the darkness comprehended it not.');
