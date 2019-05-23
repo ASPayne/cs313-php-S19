270538f9fdb158771a686b5d5e77b971cdcd1c9f
aspayne <pay12008@byui.edu>
2019-05-23 15:50:14 -0600

    week5assign beginings

--- web/MTGinterface/cardview.php
+++ web/MTGinterface/cardview.php
@@ -8,6 +8,7 @@ include 'cardquery.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="../style/main.css">
     <title>Cardviewer</title>
 </head>
 <header>
