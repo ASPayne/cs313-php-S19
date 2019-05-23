06553b1010b68a1906f07e6559ad2b52b4e98a4f
aspayne <pay12008@byui.edu>
2019-05-23 16:07:27 -0600

    week5assign beginings

--- web/MTGinterface/cardquery.php
+++ web/MTGinterface/cardquery.php
@@ -5,15 +5,15 @@ function find($id)
 {
     $resourceName = "cards";
 
-    $GLOBALS['url']      = sprintf("%s/%s/%s", "https://api.magicthegathering.io/v1", $resourceName, $id);
+    $GLOBALS['url'] = sprintf("%s/%s/%s", "https://api.magicthegathering.io/v1", $resourceName, $id);
     $response = fetch($GLOBALS['url']);
 
     return $response;
 }
 
-function fetch($url)
+function fetch($loc)
 {
-    $response = json_decode(file_get_contents($url), true);
+    $response = json_decode(file_get_contents($loc), true);
 
     return $response;
 }
