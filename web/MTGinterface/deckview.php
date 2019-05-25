94c36384262c7a2e854307e3cfbe783c851f7e40
aspayne <pay12008@byui.edu>
2019-05-25 17:44:43 -0600

    week5assign database update

--- web/MTGinterface/deckview.php
+++ web/MTGinterface/deckview.php
@@ -34,19 +34,25 @@ include '../header.php';
 
 <script>
 function showDeck(str) {
-  var xhttp;  
-  if (str == "") {
-    document.getElementById("txtHint").innerHTML = "";
-    return;
-  }
-  xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("txtHint").innerHTML = this.responseText;
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","deckquery.php?user="+str,true);
+        xmlhttp.send();
     }
-  };
-  xhttp.open("GET", "deckquery.php?user="+str, true);
-  xhttp.send();
 }
 </script>
 
